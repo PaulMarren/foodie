d0baf752f868735611b3ec091fe74d3c44d58610
PaulMarren <paul.marren.coding@gmail.com>
2025-06-16 15:06:33 +0100

    Created models for users app

--- users/models.py
+++ users/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=500, blank=True)
+    profile_image = CloudinaryField(
+        'image',
+        blank=True,  # Allow no image
+        null=True,   # Allow NULL in database
+        default=None # No default image
+    )
+
+    def __str__(self):
+        return f"{self.user.username}'s Profile"
