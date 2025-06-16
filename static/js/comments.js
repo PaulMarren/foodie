0480a96bf07e0e7a290c9beca24f461ad60b2adc
PaulMarren <paul.marren.coding@gmail.com>
2025-06-16 15:46:00 +0100

    Created js file to edit and delete comments

---
+++ static/js/comments.js
@@ -0,0 +1,52 @@
+console.log("Script loaded");
+
+const editButtons = document.getElementsByClassName("btn-edit");
+const commentText = document.getElementById("id_body");
+const commentForm = document.getElementById("commentForm");
+const submitButton = document.getElementById("submitButton");
+
+const deleteModal = new bootstrap.Modal(document.getElementById("deleteModal"));
+const deleteButtons = document.getElementsByClassName("btn-delete");
+const deleteConfirm = document.getElementById("deleteConfirm");
+
+/**
+* Initializes edit functionality for the provided edit buttons.
+* 
+* For each button in the `editButtons` collection:
+* - Retrieves the associated comment's ID upon click.
+* - Fetches the content of the corresponding comment.
+* - Populates the `commentText` input/textarea with the comment's content for editing.
+* - Updates the submit button's text to "Update".
+* - Sets the form's action attribute to the `edit_comment/{commentId}` endpoint.
+*/
+for (let button of editButtons) {
+  button.addEventListener("click", (e) => {
+    let commentId = e.target.getAttribute("data-comment_id");
+    const recipeSlug = e.target.getAttribute("data-recipe_slug");
+    let commentContent = document.getElementById(`comment${commentId}`).innerText;
+    commentText.value = commentContent;
+    submitButton.innerText = "Update";
+    commentForm.setAttribute("action", `/recipe/${recipeSlug}/comment/${commentId}/edit/`);
+     console.log("Editing comment:", commentId, "for recipe:", recipeSlug);  // Debug
+  });
+}
+
+/**
+* Initializes deletion functionality for the provided delete buttons.
+* 
+* For each button in the `deleteButtons` collection:
+* - Retrieves the associated comment's ID upon click.
+* - Updates the `deleteConfirm` link's href to point to the 
+* deletion endpoint for the specific comment.
+* - Displays a confirmation modal (`deleteModal`) to prompt 
+* the user for confirmation before deletion.
+*/
+for (let button of deleteButtons) {
+  button.addEventListener("click", (e) => {
+    let commentId = e.target.getAttribute("data-comment_id");
+    const recipeSlug = e.target.getAttribute("data-recipe_slug");
+    deleteConfirm.href = `/recipe/${recipeSlug}/comment/${commentId}/delete/`;
+    deleteModal.show();
+        console.log("Deleting comment:", commentId, "from recipe:", recipeSlug);  // Debug
+  });
+}
