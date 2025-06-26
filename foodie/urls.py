3259bbd82632391effed6b5ce138ef043ba263d1
PaulMarren <paul.marren.coding@gmail.com>
2025-06-26 16:26:45 +0100

    Cleaned up template files and added comments

--- foodie/urls.py
+++ foodie/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("accounts/", include("allauth.urls")),
     path('users/', include('users.urls')),
-    path('', include("recipes.urls")), 
+    path('', include("recipes.urls")),
 ]
